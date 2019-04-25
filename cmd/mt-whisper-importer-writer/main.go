84217dea0a7fb2eb7975b3925d8e574c60e1da25
Mauro Stettler <mauro.stettler@gmail.com>
2019-04-25 23:00:21 +0000

    copy the argument parsing from metrictank
    we want to be able to configure the mt-whisper-importer-writer via env
    variables, because that makes it much easier and cleaner when
    configuring it from HM-API. this commit basically copies the way how
    metrictank is configured into the writer utility, so it now also
    supports environment variables and the metrictank config file.

--- cmd/mt-whisper-importer-writer/main.go
+++ cmd/mt-whisper-importer-writer/main.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/grafana/globalconf"
 	"github.com/grafana/metrictank/mdata"
 	bigTableStore "github.com/grafana/metrictank/store/bigtable"
 	cassandraStore "github.com/grafana/metrictank/store/cassandra"
@@ -31,43 +32,13 @@ const (
 )
 
 var (
-	globalFlags = flag.NewFlagSet("global config flags", flag.ExitOnError)
-
-	exitOnError = globalFlags.Bool(
-		"exit-on-error",
-		true,
-		"Exit with a message when there's an error",
-	)
-	verbose = globalFlags.Bool(
-		"verbose",
-		false,
-		"More detailed logging",
-	)
-	httpEndpoint = globalFlags.String(
-		"http-endpoint",
-		"127.0.0.1:8080",
-		"The http endpoint to listen on",
-	)
-	ttlsStr = globalFlags.String(
-		"ttls",
-		"35d",
-		"list of ttl strings used by MT separated by ','",
-	)
-	partitionScheme = globalFlags.String(
-		"partition-scheme",
-		"bySeries",
-		"method used for partitioning metrics. This should match the settings of tsdb-gw. (byOrg|bySeries)",
-	)
-	uriPath = globalFlags.String(
-		"uri-path",
-		"/chunks",
-		"the URI on which we expect chunks to get posted",
-	)
-	numPartitions = globalFlags.Int(
-		"num-partitions",
-		1,
-		"Number of Partitions",
-	)
+	confFile        = flag.String("config", "/etc/metrictank/metrictank.ini", "configuration file path")
+	exitOnError     = flag.Bool("exit-on-error", true, "Exit with a message when there's an error")
+	httpEndpoint    = flag.String("http-endpoint", "127.0.0.1:8080", "The http endpoint to listen on")
+	ttlsStr         = flag.String("ttls", "35d", "list of ttl strings used by MT separated by ','")
+	partitionScheme = flag.String("partition-scheme", "bySeries", "method used for partitioning metrics. This should match the settings of tsdb-gw. (byOrg|bySeries)")
+	uriPath         = flag.String("uri-path", "/chunks", "the URI on which we expect chunks to get posted")
+	numPartitions   = flag.Int("num-partitions", 1, "Number of Partitions")
 
 	version = "(none)"
 )
@@ -87,78 +58,36 @@ func init() {
 }
 
 func main() {
+	flag.Parse()
 
-	flag.Usage = func() {
-		fmt.Println("mt-whisper-importer-writer")
-		fmt.Println()
-		fmt.Println("Opens an endpoint to send data to, which then gets stored in the MT internal DB(s)")
-		fmt.Println()
-		fmt.Printf("Usage:\n\n")
-		fmt.Printf("  mt-whisper-importer-writer [global config flags] <storetype> [store config flags] <idxtype> [idx config flags]\n\n")
-		fmt.Printf("global config flags:\n\n")
-		globalFlags.PrintDefaults()
-		fmt.Println()
-		fmt.Printf("storetype: only 'cass' and 'bt' supported\n\n")
-		fmt.Printf("cass store config flags:\n\n")
-		cassandraStore.ConfigSetup().PrintDefaults()
-		fmt.Println()
-		fmt.Printf("bt store config flags:\n\n")
-		bigTableStore.ConfigSetup().PrintDefaults()
-		fmt.Println()
-		fmt.Printf("idxtype: only 'cass' and 'bt' supported\n\n")
-		fmt.Printf("cass index config flags:\n\n")
-		cassandra.ConfigSetup().PrintDefaults()
-		fmt.Println()
-		fmt.Printf("bt index config flags:\n\n")
-		bigtable.ConfigSetup().PrintDefaults()
-		fmt.Println()
-		fmt.Println("EXAMPLES:")
-		fmt.Println("mt-whisper-importer-writer -exit-on-error=true -fake-avg-aggregates=true -http-endpoint=0.0.0.0:8080 -num-partitions=8 -partition-scheme=bySeries -ttls=8d,2y -uri-path=/chunks -verbose=true cass -addrs=192.168.0.1 -keyspace=mydata -window-factor=20 cass -hosts=192.168.0.1:9042 -keyspace=mydata")
+	// Only try and parse the conf file if it exists
+	path := ""
+	if _, err := os.Stat(*confFile); err == nil {
+		path = *confFile
 	}
-
-	if len(os.Args) == 2 && (os.Args[1] == "-h" || os.Args[1] == "--help") {
-		flag.Usage()
-		os.Exit(0)
-	}
-
-	var storeI, indexI int
-	var useStore, useIndex storeType
-	for i, v := range os.Args {
-		if v == "cass" || v == "bt" {
-			if storeI == 0 {
-				storeI = i
-				if v == "cass" {
-					useStore = cass
-				} else {
-					useStore = bt
-				}
-			} else if indexI == 0 {
-				indexI = i
-				if v == "cass" {
-					useStore = cass
-				} else {
-					useStore = bt
-				}
-			} else {
-				fmt.Println("store & index types can only be specified once for each")
-				flag.Usage()
-				os.Exit(1)
-			}
-		}
-	}
-
-	if indexI == 0 {
-		fmt.Println("store & index types need to be specified, they can be \"cass\" or \"bt\"")
-		flag.Usage()
+	config, err := globalconf.NewWithOptions(&globalconf.Options{
+		Filename:  path,
+		EnvPrefix: "IMPORTER_",
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "FATAL: configuration file error: %s", err)
 		os.Exit(1)
 	}
 
-	globalFlags.Parse(os.Args[1:storeI])
-	if *verbose {
-		log.SetLevel(log.DebugLevel)
-	} else {
-		log.SetLevel(log.InfoLevel)
-	}
+	mdata.ConfigSetup()
+	cassandra.ConfigSetup()
+	cassandraStore.ConfigSetup()
+	bigtable.ConfigSetup()
+	bigTableStore.ConfigSetup()
+
+	config.ParseAll()
+	cluster.Init("mt-whisper-importer-writer", version, time.Now(), "http", int(80))
+
+	mdata.ConfigProcess()
+	cassandra.ConfigProcess()
+	cassandraStore.ConfigSetup()
+	bigtable.ConfigProcess()
+	bigTableStore.ConfigProcess(mdata.MaxChunkSpan())
 
 	splits := strings.Split(*ttlsStr, ",")
 	ttls := make([]uint32, 0)
@@ -166,26 +95,33 @@ func main() {
 		ttls = append(ttls, dur.MustParseNDuration("ttl", split))
 	}
 
+	if (cassandraStore.CliConfig.Enabled && bigTableStore.CliConfig.Enabled) || !(cassandraStore.CliConfig.Enabled || bigTableStore.CliConfig.Enabled) {
+		log.Fatalf("exactly 1 backend store plugin must be enabled. cassandra: %t bigtable: %t", cassandraStore.CliConfig.Enabled, bigTableStore.CliConfig.Enabled)
+	}
+	if (cassandra.CliConfig.Enabled && bigtable.CliConfig.Enabled) || !(cassandra.CliConfig.Enabled || bigtable.CliConfig.Enabled) {
+		log.Fatalf("exactly 1 backend index plugin must be enabled. cassandra: %t bigtable: %t", cassandra.CliConfig.Enabled, bigtable.CliConfig.Enabled)
+	}
+
 	var index idx.MetricIndex
-	var err error
-	switch useIndex {
-	case cass:
-		index = newCassandraIndex(os.Args[storeI+1 : indexI])
-	case bt:
-		index = newBigTableIndex(os.Args[storeI+1 : indexI])
+	if cassandra.CliConfig.Enabled {
+		index = cassandra.New(cassandra.CliConfig)
+	}
+	if bigtable.CliConfig.Enabled {
+		index = bigtable.New(bigtable.CliConfig)
 	}
 
 	var store mdata.Store
-	switch useStore {
-	case cass:
-		store, err = newCassandraStore(ttls, os.Args[indexI+1:])
+	if cassandraStore.CliConfig.Enabled {
+		store, err = cassandraStore.NewCassandraStore(cassandraStore.CliConfig, mdata.TTLs())
 		if err != nil {
-			log.Fatalf("Failed to initialize cassandra store: %s", err)
+			log.Fatalf("failed to initialize cassandra backend store. %s", err)
 		}
-	case bt:
-		store, err = newBigTableStore(ttls, os.Args[indexI+1:])
+	}
+	if bigTableStore.CliConfig.Enabled {
+		schemaMaxChunkSpan := mdata.MaxChunkSpan()
+		store, err = bigTableStore.NewStore(bigTableStore.CliConfig, mdata.TTLs(), schemaMaxChunkSpan)
 		if err != nil {
-			log.Fatalf("Failed to initialize the bigtable store: %s", err)
+			log.Fatalf("failed to initialize bigtable backend store. %s", err)
 		}
 	}
 
@@ -194,18 +130,17 @@ func main() {
 		panic(fmt.Sprintf("Failed to instantiate partitioner: %q", err))
 	}
 
-	cluster.Init("mt-whisper-importer-writer", version, time.Now(), "http", int(80))
+	index.Init()
 
 	server := &Server{
 		partitioner: p,
 		index:       index,
+		store:       store,
 		HTTPServer: &http.Server{
 			Addr:        *httpEndpoint,
 			ReadTimeout: 10 * time.Minute,
 		},
-		store: store,
 	}
-	server.index.Init()
 
 	http.HandleFunc(*uriPath, server.chunksHandler)
 	http.HandleFunc("/healthz", server.healthzHandler)
